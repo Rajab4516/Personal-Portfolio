72da6dfca3859aa369057fee45b3b279d7c98220
Rajab Asghar <alfatahrays@gamil.com>
2024-11-11 18:18:23 +0500

    Starting the Project

---
+++ src/Components/MainBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainBanner.css';
+export default function MainBanner() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="main-container-col-1"><h1>Hello</h1></div>
+        <div className="main-container-col-2"></div>
+      </div>
+    </>
+  )
+}
