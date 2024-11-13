c03eba0666629b75357e4919e7205a8c1d4e6b62
Rajab Asghar <alfatahrays@gamil.com>
2024-11-13 18:24:55 +0500

    Creating a Navbar and Banner Section without responsiveness

--- src/Components/MainBanner.js
+++ src/Components/MainBanner.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './MainBanner.css';
+import Navbar from './Navbar';
+import BannerImage from './images/image2.png'
 export default function MainBanner() {
   return (
     <>
-      <div className="main-container">
-        <div className="main-container-col-1"><h1>Hello</h1></div>
-        <div className="main-container-col-2"></div>
+        <div className="navbar-main-container">
+          <Navbar></Navbar>
+        </div>
+      <div className="main-banner-container">
+        <div className="main-banner-container-col-1">
+          <div className="main-banner-container-col-1-content">
+            <img src={BannerImage} alt="Banner-Image" id='banner-image'/>
+          </div>
+        </div>
+        <div className="main-banner-container-col-2">
+        <div className="main-banner-container-col-2-content">
+          <p><span id='rajab-name'>&lt;Hello! I'm Rajab/&gt;</span> a passionate Web Developer. I create sleek, functional, and user-focused websites that bring ideas to life online. With expertise in modern web technologies, I focus on building dynamic, responsive sites that make an impact. Let’s make something great together!</p>
+        </div>
+        </div>
       </div>
     </>
   )
---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="logo-container">
+            <h4>CWR</h4>
+        </div>
+        <div className="navbar-links-container">
+            <ul>
+                <li><a href="#" className='active'>Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Contact Me</a></li>
+            </ul>
+        </div>
+      </div>
+    </>
+  )
+}
