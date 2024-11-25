f124d541b1ededf295bcf461d12b36b6081f8b9c
Rajab Asghar <alfatahrays@gamil.com>
2024-11-25 18:42:53 +0500

    Almost Complete the Skill Section

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,67 +1,116 @@
 import React from 'react';
 import './Skills.css';
-import htmlLogoIcon from './images/html1.png';
-// import cssLogoIcon from './images/css.png';
+import htmlLogoIcon from './images/html.png';
+import cssLogoIcon from './images/css.png';
+import javascriptLogoIcon from './images/javascript.png';
+import reactLogoIcon from './images/react2.png';
+import phpLogoIcon from './images/php1.png';
+import mysqlLogoIcon from './images/mysql.png';
 
 export default function Skills() {
   return (
     <>
-    {/*Skills Parent Container*/}
+    {/*Skills Parent Container Title*/}
     <div className="skills-main-container-title"><h1>My Expertise</h1></div>
-      <div className="skills-main-container">
-        {/*Skills Row 1*/}
-        <div className="skills-row-1 skills-row">
-          {/*Skills skill 1*/}
-          <div className="skills-card skill-1">
-            <div className="logo-and-title-container">
-              <img src={htmlLogoIcon} alt="HTML Logo Icon" />
-              <div className="skill-title-container">
-                <h1>HTML</h1>
-              </div>
-            </div>
-            <div className="text-container">
-              <p>I excel in crafting clean, semantic, and well-structured HTML code, laying a robust foundation for responsive and user-centric web applications. With a focus on best practices and accessibility, I ensure every project meets professional standards and delivers seamless performance.</p>
-            </div>
-            <div className="buttons-container"></div>
+    {/*Skills Parent Container*/}
+    <div className="skills-main-container">
+      <div className="row-1 row">
+        {/*Skills COL-1*/}
+        <div className="col-1 col">
+          <div className="skill-logo-container">
+            <img src={htmlLogoIcon} alt="" />
           </div>
-          {/*Skills skill 2*/}
-          <div className="skills-card skill-2">
-            <div className="logo-and-title-container"></div>
-            <div className="text-container"></div>
-            <div className="buttons-container"></div>
+          <div className="skill-text-and-button-container">
+            <div className='skill-title'><h1>HTML5</h1></div>
+            <div className="skill-description">
+              <p>Proficient in crafting semantic, accessible, and SEO-friendly HTML structures. With a strong foundation in web standards, I excel at building responsive layouts that serve as the backbone for engaging and user-centric web experiences.</p>
+            </div>
+            <div className="skill-button">
+              <button type="button">Read More</button>
+            </div>
           </div>
         </div>
-        {/*Skills Row 2*/}
-        <div className="skills-row-2 skills-row">
-          {/*Skills skill 3*/}
-          <div className="skills-card skill-3">
-            <div className="logo-and-title-container"></div>
-            <div className="text-container"></div>
-            <div className="buttons-container"></div>
+        {/*Skills COL-2*/}
+        <div className="col-2 col">
+          <div className="skill-logo-container">
+            <img src={cssLogoIcon} alt="" />
           </div>
-          {/*Skills skill 4*/}
-          <div className="skills-card skill-4">
-            <div className="logo-and-title-container"></div>
-            <div className="text-container"></div>
-            <div className="buttons-container"></div>
-          </div>
-        </div>
-        {/*Skills Row 3*/}
-        <div className="skills-row-3 skills-row">
-          {/*Skills skill 5*/}
-          <div className="skills-card skill-5">
-            <div className="logo-and-title-container"></div>
-            <div className="text-container"></div>
-            <div className="buttons-container"></div>
-          </div>
-          {/*Skills skill 6*/}
-          <div className="skills-card skill-6">
-            <div className="logo-and-title-container"></div>
-            <div className="text-container"></div>
-            <div className="buttons-container"></div>
+          <div className="skill-text-and-button-container">
+            <div className='skill-title'><h1>CSS3</h1></div>
+            <div className="skill-description">
+              <p>Skilled in CSS3, specializing in creating visually appealing, responsive, and cross-browser-compatible designs. Proficient in leveraging modern features like Flexbox, Grid, and animations to craft dynamic and engaging user interfaces.</p>
+            </div>
+            <div className="skill-button">
+              <button type="button">Read More</button>
+            </div>
           </div>
         </div>
       </div>
+      <div className="row-2 row">
+        {/*Skills COL-3*/}
+        <div className="col-3 col">
+          <div className="skill-logo-container">
+            <img src={javascriptLogoIcon} alt="" />
+          </div>
+          <div className="skill-text-and-button-container">
+            <div className='skill-title'><h1>Javascript</h1></div>
+            <div className="skill-description">
+              <p>Experienced in JavaScript, adept at building dynamic, interactive, and efficient web applications. Skilled in utilizing modern ES6+ features, DOM manipulation, and integrating APIs to deliver seamless user experiences.</p>
+            </div>
+            <div className="skill-button">
+              <button type="button">Read More</button>
+            </div>
+          </div>
+        </div>
+        {/*Skills COL-4*/}
+        <div className="col-4 col">
+          <div className="skill-logo-container">
+            <img src={reactLogoIcon} alt="" />
+          </div>
+          <div className="skill-text-and-button-container">
+            <div className='skill-title'><h1>React</h1></div>
+            <div className="skill-description">
+              <p>Proficient in React, skilled at building fast, scalable, and component-based web applications. Experienced in state management, hooks, and creating reusable UI components for seamless user experiences.</p>
+            </div>
+            <div className="skill-button">
+              <button type="button">Read More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row-3 row">
+        {/*Skills COL-5*/}
+        <div className="col-5 col">
+          <div className="skill-logo-container">
+            <img src={phpLogoIcon} alt="" />
+          </div>
+          <div className="skill-text-and-button-container">
+            <div className='skill-title'><h1>php</h1></div>
+            <div className="skill-description">
+              <p>Experienced in PHP, specializing in building robust server-side applications and dynamic websites. Proficient in integrating databases, handling APIs, and creating secure, scalable backend solutions.</p>
+            </div>
+            <div className="skill-button">
+              <button type="button">Read More</button>
+            </div>
+          </div>
+        </div>
+        {/*Skills COL-6*/}
+        <div className="col-6 col">
+          <div className="skill-logo-container">
+            <img src={mysqlLogoIcon} alt="" />
+          </div>
+          <div className="skill-text-and-button-container">
+            <div className='skill-title'><h1>MySQL</h1></div>
+            <div className="skill-description">
+              <p>Skilled in MySQL, proficient in designing and managing relational databases. Experienced in writing optimized queries, ensuring data integrity, and creating scalable database architectures for web applications.</p>
+            </div>
+            <div className="skill-button">
+              <button type="button">Read More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
